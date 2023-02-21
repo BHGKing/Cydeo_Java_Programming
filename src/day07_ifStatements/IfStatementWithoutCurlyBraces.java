87c0e63e44550e2373a2da83ccd5b5fa1c5b5d4d
BHGKing <josecantu111@gmail.com>
2023-02-21 17:04:28 -0600

    Initial Commit

--- src/day07_ifStatements/IfStatementWithoutCurlyBraces.java
+++ src/day07_ifStatements/IfStatementWithoutCurlyBraces.java
@@ -14,6 +14,53 @@ public class IfStatementWithoutCurlyBraces {
 
 
 
+        System.out.println("-----------------------------------------------");
+
+
+        //**Optional**
+
+        if(age>=21) System.out.println("Eligible");  // must have only 1 statement to be used without curly brace
+        else System.out.println("Not Eligible");
+
+
+
+
+        System.out.println("-----------------------------------------------");
+
+      /*
+        int itemNum = 1;
+
+        if (itemNum == 1)
+            System.out.println("Eggs");
+            //System.out.println("Orange");
+        else if (itemNum == 2)
+            System.out.println("Milk");
+            //System.out.println("Onion");
+        else
+            System.out.println("Apple");
+           // System.out.println("Cherry");
+     */
+
+        int day = 7; // 1 ~ 7
+
+        if (day == 1) System.out.println("Monday");
+        else if (day == 2) System.out.println("Tuesday");
+        else if (day == 3) System.out.println("Wednesday");
+        else if (day == 4) System.out.println("Thursday");
+        else if (day == 5) System.out.println("Friday");
+        else if (day == 6) System.out.println("Saturday");
+        else System.out.println("Sunday");
+
+        System.out.println("-----------------------------------------------");
+
+
+
+
+
+
+
+
+
 
     }
 }
