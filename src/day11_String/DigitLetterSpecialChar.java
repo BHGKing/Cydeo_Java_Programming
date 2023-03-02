e1395ae7fc9bc92dfd6ba5d8fd72f40af2ceae18
BHGKing <josecantu111@gmail.com>
2023-03-02 02:40:18 -0600

    Initial Commit

---
+++ src/day11_String/DigitLetterSpecialChar.java
@@ -0,0 +1,41 @@
+package day11_String;
+
+import java.util.Scanner;
+
+public class DigitLetterSpecialChar {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a word:");
+        String word = input.nextLine();
+
+        if (word.length() >= 1){  // if string has at least 1 character then run code
+
+            char f = word.charAt(0); // the first character of string
+
+            if (f >= '0' && f <= '9'){
+                System.out.println("First character is digit");
+            } else if ( f >= 'A' && f <= 'Z'){
+                System.out.println("First character is uppercase letter");
+            } else if ( f >= 'a' && f <= 'z'){
+                System.out.println("First character is lowercase letter");
+            } else {
+                System.out.println("First character is special character");
+            }
+
+        }else {  // else string is empty
+            System.out.println("String is empty");
+        }
+    }
+}
+/*
+3. Create a class named DigitLetterSpecialChar and write a program that can ask the user to enter a word
+	        - if the word starts with digits, print "first character is digit"
+	        - if the word starts with uppercase letters, print "first character is lowercase letter"
+	        - if the word starts with lowercase letters, print "first character is uppercase letter"
+	        - if the word starts with special characters, print "first character is special character"
+
+
+        	HINT: You need to check the ascii table
+ */
---
+++ src/day11_String/StringMethods2.java
@@ -0,0 +1,7 @@
+package day11_String;
+
+public class StringMethods2 {
+    public static void main(String[] args) {
+
+    }
+}
